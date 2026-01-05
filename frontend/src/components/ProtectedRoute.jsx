dcd7e2d511bcf0244e28613f478b7567b190bb3a
MdAyanBadar <158168631+MdAyanBadar@users.noreply.github.com>
2026-01-06 01:53:23 +0530

    updated ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,7 +3,9 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
 
-  if (!token) return <Navigate to="/login" replace />;
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
 
   return children;
 }
